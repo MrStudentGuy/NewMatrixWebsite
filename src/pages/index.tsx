d32253c2b27eceb11fdc22f1fb9b33733bcc77b9
Aryan Pai <aryan.pai7@icloud.com>
2023-11-03 18:49:24 +0530

    Fixed home page linking

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
     return (
@@ -32,20 +33,20 @@ export default function Home() {
                         <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
                             <h2 className="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">Meet The Team</h2>
                             <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">Team Matrix attracts the most passionate members in the community. We&apos;re a family of aspiring engineers, innovators, and problem-solvers, all united by our love for technology.</p>
-                            <a href="#" className="text-blue-400 hover:underline font-medium text-lg inline-flex items-center">See
+                            <Link href="/meet-the-team" className="text-blue-400 hover:underline font-medium text-lg inline-flex items-center">See
                                 <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                                 </svg>
-                            </a>
+                            </Link>
                         </div>
                         <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
                             <h2 className="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">Contact Us</h2>
                             <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">Want to conduct an outreach session with us, collaborate in anyway, or just chat?</p>
-                            <a href="#" className="text-blue-400 hover:underline font-medium text-lg inline-flex items-center">Get In Touch
+                            <Link href="/contact" className="text-blue-400 hover:underline font-medium text-lg inline-flex items-center">Get In Touch
                                 <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                                 </svg>
-                            </a>
+                            </Link>
                         </div>
                     </div>
                 </section>
